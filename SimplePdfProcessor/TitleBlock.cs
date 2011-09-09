1f8e995b80d840d364233d3a9051af5ca456892d
bargool <bargool@gmail.com>
2011-09-09 23:22:17 +0400

    Added interface providing titleblocks of drawings to fill them. Still in work

---
+++ SimplePdfProcessor/TitleBlock.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: aleksey
+ * Date: 09.09.2011
+ * Time: 22:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace SimplePdfProcessor
+{
+	/// <summary>
+	/// Description of TitleBlock.
+	/// </summary>
+	public class TitleBlock
+	{
+		public int pageIndex {get; set;}
+		int dX;
+		int dY;
+		int Width;
+		int Height;
+		
+		public TitleBlock()
+		{
+		}
+		
+		public void Draw()
+		{
+			
+		}
+	}
+}
